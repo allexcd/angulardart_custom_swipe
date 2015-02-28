af7ffe2713141acad03d9d618ca5a164c5ab1ba8
AlexCD <email@alexcd.eu>
2015-02-28 13:33:40 +0200

    Add time as a third variable for swipe detection
    - Add the time spent between touchStart and touchEnd as a third variable
    for a swipe detection.
    - Move attributes for swipe detection at beginning of the class for easy
    customization
    - Set default values of swipe detection attributes to: distance: 50 px,
    angle: 20 degrees, time: 300 milliseconds

--- angulardart_custom_swipe/CHANGELOG.md
+++ angulardart_custom_swipe/CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.1.4
+- Add the time spent between touchStart and touchEnd as a third variable for a swipe detection.
+- Move attributes for swipe detection at beginning of the class for easy customization
+- Set default values of swipe detection attributes to: distance: 50 px, angle: 20 degrees, time: 300 milliseconds
+
 ## 0.1.3
 - Fix for scroll not triggered when not swiping
 
--- angulardart_custom_swipe/lib/decorator/detect_swipe.dart
+++ angulardart_custom_swipe/lib/decorator/detect_swipe.dart
@@ -2,12 +2,21 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 part of angulardart.custom.swipe;
 
-class detectSwipe{
+class detectSwipe {
   Map _startCoord, _endCoord;
   Function swipeRight, swipeLeft;
+  double _startTime, _stopTime;
+
+  // set swipe attributes
+  // distance (px), angle (degrees), duration (ms)
+  int _swipeDistance = 50, _swipeAngle = 20, _swipeTime = 300;
 
   void setConsoleMsg(msg) {
     dom.window.console.info(msg);
+  }
+
+  double getTimeInMs() {
+    return new DateTime.now().millisecondsSinceEpoch.toDouble();
   }
 
   double _getTheta(Map endCoord, Map startCoord) {
@@ -24,7 +33,7 @@ class detectSwipe{
     return theta.abs();
   }
 
-  double _getSwipeTheta(){
+  double _getSwipeTheta() {
     return _getTheta(_endCoord, _startCoord);
   }
 
@@ -44,9 +53,9 @@ class detectSwipe{
     // When a touchmove event occurs if the browser decides that this is a scroll event
     // it will fire touchcancel and never fire touchend.
     // But the scroll must pe allowed on some conditions, if moving the finger up and down on the screen
-    target.onTouchMove.listen((e){
-      double moveTheta = _getTheta(_getTouchCoordinates(e), _startCoord);
-      if(moveTheta > 30 && moveTheta < 150){
+    target.onTouchMove.listen((e) {
+      double angle = _getTheta(_getTouchCoordinates(e), _startCoord);
+      if (angle > 30 && angle < 150) {
         setConsoleMsg('scroll');
       } else {
         setConsoleMsg('disable scroll');
@@ -57,32 +66,40 @@ class detectSwipe{
   }
 
   void _makeSwipe() {
+    int distance = _startCoord["x"] - _endCoord["x"];
+    double time = _stopTime - _startTime;
+    double angle = _getSwipeTheta();
+
     if (_startCoord != null && _endCoord != null) {
-      if ((_startCoord["x"] - _endCoord["x"]) < -10) {
-        if (swipeRight != null) {
-          if (_getSwipeTheta() >= 0 && _getSwipeTheta() <= 10) {
-            setConsoleMsg('swiped right');
-            swipeRight();
-          }
+      //TODO: check if callback is undefined. For now, the condition is met even if a callback is not provided
+      if (swipeRight != null) {
+        //swipe right (negative values)
+        if (time <= _swipeTime && distance < -_swipeDistance && angle >= 0 && angle <= _swipeAngle) {
+          setConsoleMsg('swiped right');
+          swipeRight();
         }
-      } else if ((_startCoord["x"] - _endCoord["x"]) > 10) {
-        if (swipeLeft != null) {
-          if (_getSwipeTheta() >= 170 && _getSwipeTheta() <= 180) {
-            setConsoleMsg('swiped left');
-            swipeLeft();
-          }
+      }
+
+      //TODO: check if callback is undefined. For now, the condition is met even if a callback is not provided
+      if (swipeLeft != null) {
+        //swipe right (positive values)
+        if (time <= _swipeTime && distance > _swipeDistance && angle >= (180 - _swipeAngle) && angle <= 180) {
+          setConsoleMsg('swiped left');
+          swipeLeft();
         }
       }
     }
   }
 
   void _touchStart(dom.TouchEvent ev) {
-    setConsoleMsg('touchStart');
+//    setConsoleMsg('touchStart');
+    _startTime = getTimeInMs();
     _startCoord = _getTouchCoordinates(ev);
   }
 
   void _touchEnd(dom.TouchEvent ev) {
-    setConsoleMsg('touchEnd');
+//    setConsoleMsg('touchEnd');
+    _stopTime = getTimeInMs();
     _endCoord = _getTouchCoordinates(ev);
     _makeSwipe();
   }
